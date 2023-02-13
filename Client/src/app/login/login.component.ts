e7c1b12be115530ea4d261e376361c6d81ac9770
GoodJoe2049 <marcosrod171@gmail.com>
2023-02-13 15:52:57 -0600

    Basic login page structure created

--- Client/src/app/login/login.component.ts
+++ Client/src/app/login/login.component.ts
@@ -6,8 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  username : string = "";
+  password : string = "";
+  show : boolean = false;
 
   constructor() { }
+
+  submit(){
+    //console.log("user name is " + this.username)
+    //must add the check for user + password
+    //then must reroute to appropriate dashboard based on student/staff
+    this.clear();
+  }
+
+  clear(){
+    this.username ="";
+    this.password = "";
+    this.show = true;
+  }
 
   ngOnInit(): void {
   }
